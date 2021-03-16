75cff348eee6f64bf9a3745b4edaeaa45027dfaa
prognroller <prognroller@gmail.com>
2021-03-16 22:13:42 +0200

    File copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,75 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrNegativeParams        = errors.New("negative value of offset or limit")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 || limit < 0 {
+		return ErrNegativeParams
+	}
+
+	copyFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer copyFile.Close()
+
+	copyFileStat, err := copyFile.Stat()
+	if err != nil {
+		return err
+	}
+	if copyFileStat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	newFile, err := ioutil.TempFile("", "fedya")
+	if err != nil {
+		return err
+	}
+
+	_, err = copyFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	if (limit == 0) || (limit > copyFileStat.Size()-offset) {
+		limit = copyFileStat.Size() - offset
+	}
+
+	reader := io.LimitReader(copyFile, limit)
+
+	bar := pb.Full.Start64(limit)
+
+	barReader := bar.NewProxyReader(reader)
+
+	_, err = io.Copy(newFile, barReader)
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
+
+	dirPath, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	newPath := fmt.Sprintf("%v/%v", dirPath, toPath)
+	err = os.Rename(newFile.Name(), newPath)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		name             string
+		fromPath, toPath string
+		limit, offset    int64
+	}{
+		{name: "Negative offset", fromPath: "testdata/input.txt", toPath: "out.txt", offset: -1, limit: 0},
+		{name: "Negative limit", fromPath: "testdata/input.txt", toPath: "out.txt", offset: 0, limit: -1},
+		{name: "Offset larger than file", fromPath: "testdata/input.txt", toPath: "out.txt", offset: 10000, limit: 0},
+		{name: "Unsupported file", fromPath: "testdata/input1.txt", toPath: "out.txt", offset: 0, limit: 0},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := Copy(test.fromPath, test.toPath, test.offset, test.limit)
+			require.Error(t, err)
+		})
+	}
 }
